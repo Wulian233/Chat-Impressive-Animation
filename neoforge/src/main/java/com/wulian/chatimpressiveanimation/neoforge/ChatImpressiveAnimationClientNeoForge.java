7f445c93d64d0aced766f8d67a473e01b9a146c5
Wulian233 <1055917385@qq.com>
2025-08-17 15:08:29 +0800

    Release 1.4.0

--- neoforge/src/main/java/com/wulian/chatimpressiveanimation/neoforge/ChatImpressiveAnimationClientNeoForge.java
+++ neoforge/src/main/java/com/wulian/chatimpressiveanimation/neoforge/ChatImpressiveAnimationClientNeoForge.java
@@ -5,6 +5,7 @@ import com.wulian.chatimpressiveanimation.config.ConfigUtil;
 import com.wulian.chatimpressiveanimation.config.ModConfigs;
 import me.shedaniel.autoconfig.AutoConfig;
 import net.minecraft.client.gui.screen.Screen;
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.ModList;
 import net.neoforged.fml.common.Mod;
@@ -13,12 +14,14 @@ import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
 import java.util.function.Function;
 
-@Mod(ChatImpressiveAnimation.MOD_ID)
+@Mod(value = ChatImpressiveAnimation.MOD_ID, dist = Dist.CLIENT)
 public class ChatImpressiveAnimationClientNeoForge {
     public ChatImpressiveAnimationClientNeoForge() {
         if (FMLLoader.getDist().isClient()) {
 			ConfigUtil.getConfig();
+
 			registerConfigScreen(ChatImpressiveAnimation.MOD_ID, screen -> AutoConfig.getConfigScreen(ModConfigs.class, screen).get());
+
 			ChatImpressiveAnimation.LOGGER.info("Chat Impressive Animation is loaded!");
         }
     }
