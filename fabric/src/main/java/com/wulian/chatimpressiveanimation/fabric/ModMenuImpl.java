c374b75b463490b54edc168edee0b92fc918787b
Wulian233 <1055917385@qq.com>
2025-12-14 20:09:37 +0800

    Port to 1.21.11

--- fabric/src/main/java/com/wulian/chatimpressiveanimation/fabric/ModMenuImpl.java
+++ fabric/src/main/java/com/wulian/chatimpressiveanimation/fabric/ModMenuImpl.java
@@ -3,11 +3,11 @@ package com.wulian.chatimpressiveanimation.fabric;
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
 import com.wulian.chatimpressiveanimation.config.ModConfigs;
-import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.AutoConfigClient;
 
 public class ModMenuImpl implements ModMenuApi {
 	@Override
 	public ConfigScreenFactory<?> getModConfigScreenFactory() {
-		return screen -> AutoConfig.getConfigScreen(ModConfigs.class, screen).get();
+		return screen -> AutoConfigClient.getConfigScreen(ModConfigs.class, screen).get();
 	}
 }
